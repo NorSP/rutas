b7a132c77bd1fdb936800166d4ebfde1dfcb5aea
NorSP <norbissilvera@gmail.com>
2022-10-20 15:23:08 +0000

    sin

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,117 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+    all_users = User.query.all()
+    results = list(map(lambda item: item.serialize(), all_users))
+    return jsonify(results), 200
+
+@app.route('/users', methods=['POST'])
+def usersPost():
+    body = json.loads(request.data)
+    query_user = User.query.filter_by(email=body["email"]).first()
+    favorites_user = Favorite.query.first()
+    if query_user is None:
+        new_user = User(email=body["email"], password=body["password"], name=body["name"], is_active=bool(body["is_active"]), id=len(User.query.all())+1)
+        db.session.add(new_user)
+        db.session.commit()
+        new_table_favorite = Favorite(favorite_lista="",id=len(Favorite.query.all())+1)
+        db.session.add(new_table_favorite)
+        db.session.commit()
+
+        response_body = {
+            "msg" : "created user"
+        }
+        return jsonify(response_body), 200
+    
+
+    
+    return jsonify("usuario existente")
+
+
+
+@app.route('/users/<int:users_id>', methods=['GET'])
+def usersId(users_id):
+    
+    usuario = User.query.filter_by(id = users_id).first()
+      #no se mapea en este caso
+    return jsonify(usuario.serialize()), 200
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    
+    planetas = Planets.query.all()
+    print(planetas)
+    results = list(map(lambda item: item.serialize(), planetas))
+    return jsonify(results), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def planetsId(planets_id):
+    
+    planetasId = Planets.query.filter_by(id = planets_id).first()
+      #no se mapea en este caso
+    return jsonify(planetasId.serialize()), 200
+
+@app.route('/users/<int:users_id>/favorite', methods=['GET'])
+def favoritesId(users_id):
+    favoritesUser = Favorite.query.filter_by(id = users_id).first()
+    if favoritesUser is None:
+        obj = {
+            "msg": "error; the user does not exist"
+        }
+        return jsonify(obj), 404
+    obj = {
+        "favorite_lista": dict(favoritesUser.serialize())["favorite_lista"].split("$$")
     }
 
-    return jsonify(response_body), 200
+    
+    return jsonify(obj), 200
+
+@app.route('/users/<int:users_id>/favorite', methods=['PUT'])
+def favoritePost(users_id):
+
+    body = json.loads(request.data)
+    favorites_user = Favorite.query.filter_by(id = users_id).first()
+    
+    
+    if body["favorite"] in dict(favorites_user.serialize())["favorite_lista"]:
+        
+       
+        aux = dict(favorites_user.serialize())["favorite_lista"]
+        aux = aux.split("$$"+body["favorite"])
+        aux = ''.join(aux)
+        favorites_user.favorite_lista = aux 
+        db.session.commit()
+
+        return jsonify({"msg": "the favorite has been deleted successfully" }), 200
+        
+    elif favorites_user is None and body is None:
+        return jsonify({"msg": "There is no list for this user"}), 404
+    favorites_user.favorite_lista = dict(favorites_user.serialize())["favorite_lista"]+"$$"+body["favorite"]
+    db.session.commit()
+    return jsonify({"msg": "The favorite has been added successfully"}), 404
+
+
+    
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+    
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,74 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', lazy=True)
+
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name" : self.email,
+            "favorites": list(map(lambda x: x.serialize(), self.favorites))
+            
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+   
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(250), nullable=False)
+    populaton = db.Column(db.String(250), nullable=False)
+    rotation_period = db.Column(db.String(250), nullable=False)
+    orbital_period = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    terrain_grasslands = db.Column(db.String(250), nullable=False)
+    surface_water = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+           
+            "name": self.name,
+            "populaton": self.populaton,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "terrain_grasslands": self.terrain_grasslands,
+            "surface_water": self.surface_water,
+            "climate": self.climate,
+
+
+            
+            # do not serialize the password, its a security breach
+        }
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    favorite_lista = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+    def serialize(self):
+        return {
+            "id": self.id,
+            "favorite_lista": self.favorite_lista
+            
+            
             # do not serialize the password, its a security breach
         }
